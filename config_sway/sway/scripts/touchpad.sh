7a55e260741ed1684c6f4a2423ed87b156d9c64a
Krant274 <thctan274@proton.me>
2025-02-15 22:50:59 +0700

    .

--- config_sway/sway/scripts/touchpad.sh
+++ config_sway/sway/scripts/touchpad.sh
@@ -1,39 +1,13 @@
 #!/usr/bin/env bash
 
-HYPRLAND_DEVICE="asue120a:00-04f3:319b-touchpad"
+#use  'swaymsg -t get_inputs' to get  id
 
-if [ -z "$XDG_RUNTIME_DIR" ]; then
-  export XDG_RUNTIME_DIR="/run/user/$(id -u)"
-fi
-
-export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
-
-enable_touchpad() {
-  printf "true" >"$STATUS_FILE"
-
-  dunstify --replace=11112 -a 'no' "Touchpad: Enabled"
-
-  hyprctl keyword "device[$HYPRLAND_DEVICE]:enabled" true
-}
-
-disable_touchpad() {
-  printf "false" >"$STATUS_FILE"
-
-  dunstify --replace=11112 -a 'no' "Touchpad: Disabled "
-
-  hyprctl keyword "device[$HYPRLAND_DEVICE]:enabled" false
-}
-
-notify_cmd() {
-  dunstify --replace=11112 -a 'no' --hints=int:value:"Touchpad: " "$(cat "$STATUS_FILE")"
-}
-
-if ! [ -f "$STATUS_FILE" ]; then
-  enable_touchpad
+if [ ! -f /tmp/touchpad.lock ]; then
+  dunstify --replace=11112 -a 'no' "Touchpad: Disabled"
+  swaymsg input "1267:12699:ASUE120A:00_04F3:319B_Touchpad" events disabled &
+  touch /tmp/touchpad.lock
 else
-  if [ "$(cat "$STATUS_FILE")" = "true" ]; then
-    disable_touchpad
-  elif [ "$(cat "$STATUS_FILE")" = "false" ]; then
-    enable_touchpad
-  fi
+  dunstify --replace=11112 -a 'no' "Touchpad: Enabled"
+  swaymsg input "1267:12699:ASUE120A:00_04F3:319B_Touchpad" events enabled &
+  rm -f /tmp/touchpad.lock
 fi
